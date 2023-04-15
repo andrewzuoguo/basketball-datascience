a537e9de688b49adcb0f195541bcc2926d8a141b
andrewzuoguo <andrewzguo@gmail.com>
2023-04-15 17:57:49 -0400

    Add download_player_active.py Downloads regular season player statistics for all active NBA players

---
+++ data/download_players_active.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from nba_api.stats.endpoints import playercareerstats
+from connect_sqlite import connect
+
+
+def download_allplayerseasons(player_ids):
+    """
+    Download regular season player statistics for all active NBA players
+    :param player_ids: List of ids of active players
+    :return: DataFrame of all regular season player statistics for the given player IDs, with additional columns for PPG, RPG, and APG
+    """
+
+    allplayerseasons = pd.DataFrame()
+
+    while len(player_ids) > 0:
+        try:
+            id = player_ids[-1]
+
+            if len(allplayerseasons) == 0:
+                print(f'Initiating download with player {id}...')
+                playercareer = playercareerstats.PlayerCareerStats(player_id=id, timeout=100)
+                allplayerseasons = playercareer.get_data_frames()[0]
+
+            elif id not in list(allplayerseasons['PLAYER_ID']):
+                print(f'Downloading player {id}...')
+                playercareer = playercareerstats.PlayerCareerStats(player_id=id, timeout=100)
+                allplayerseasons = pd.concat([allplayerseasons, playercareer.get_data_frames()[0]])
+
+            else:
+                print(f'Already have player {id}.')
+
+            player_ids.pop()
+
+        except Exception as e:
+            print(f'Error downloading player {id}: {e}')
+            continue
+
+        else:
+            print(f'Successfully downloaded player {id}.')
+
+    allplayerseasons['PPG'] = allplayerseasons['PTS']/allplayerseasons['GP']
+    allplayerseasons['RPG'] = allplayerseasons['REB']/allplayerseasons['GP']
+    allplayerseasons['APG'] = allplayerseasons['AST']/allplayerseasons['GP']
+
+    return allplayerseasons
+
+
+if __name__ == '__main__':
+
+    with connect() as conn:
+        player_info = pd.read_sql_query("SELECT id, full_name, first_name, last_name, is_active FROM PLAYER_LIST_ACTIVE", conn, params=())
+        player_ids = list(player_info['id'])
+
+        allplayerseasons = download_allplayerseasons(player_ids)
+        allplayerseasons = allplayerseasons.merge(player_info, how='right', left_on='PLAYER_ID', right_on='id')
+
+        allplayerseasons.to_sql('PLAYERS_ACTIVE', conn, if_exists='replace')
