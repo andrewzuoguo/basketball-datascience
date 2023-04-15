4b7f6ec5698f4c3fb30c2d63e2e8ca66125d1b24
andrewzuoguo <andrewzguo@gmail.com>
2023-04-15 12:33:41 -0400

    Improve error handling of connect() Add comments

--- data/connect_sqlite.py
+++ data/connect_sqlite.py
@@ -2,13 +2,26 @@ import sqlite3
 from datetime import datetime
 
 def connect():
+    """
+    Establishes a connection to the 'nba-data.db' database.
+
+    Returns:
+    conn (sqlite3.Connection): a Connection object representing the database connection
+    """
     try:
         conn = sqlite3.connect("nba-data.db")
-    except Exception as error:
-        print(error)
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+        raise e
 
     return conn
 
-def now():
-    now = datetime.now()
-    return now.strftime("%m/%d/%Y, %H:%M:%S")
+def get_current_time():
+    """
+    Returns the current date and time in the format 'MM/DD/YYYY, HH:MM:SS'.
+
+    Returns:
+    (str): a string representing the current date and time
+    """
+    current_time = datetime.now()
+    return current_time.strftime("%m/%d/%Y, %H:%M:%S")
