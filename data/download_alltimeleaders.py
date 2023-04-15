6d2e95d499d51f1896706fd1387c78f2020462e7
andrewzuoguo <andrewzguo@gmail.com>
2023-04-15 12:31:23 -0400

    Add comments and minor updates to download_alltimeleaders.py

--- data/download_alltimeleaders.py
+++ data/download_alltimeleaders.py
@@ -3,6 +3,10 @@ from nba_api.stats.endpoints import alltimeleadersgrids
 from connect_sqlite import connect
 
 def download_alltimeleaders():
+    """
+    Download data of all-time NBA leaders and return as a pandas DataFrame.
+    """
+
     leaders = alltimeleadersgrids.AllTimeLeadersGrids().get_data_frames()
 
     desc = ['GP', 'PTS', 'AST', 'STL', 'OREB', 'DREB', 'REB', 'BLK', 'FGM', 'FGA', 'FGPCT', 'TOV', 'FG3M', 'FG3A', 'FG3PCT', 'PF', 'FTM', 'FTA', 'FTPCT']
@@ -19,20 +23,25 @@ def download_alltimeleaders():
 
     out['IS_ACTIVE'] = out['IS_ACTIVE'].apply(lambda x: False if x=='N' else True)
 
-    out = out.astype({
+    COLUMN_TYPES = {
         'PLAYER_ID': int,
         'PLAYER_NAME': str,
         'VALUE': int,
         'RANK': int,
         'IS_ACTIVE': bool,
         'TYPE': str
-    })
+    }
+    out = out.astype(COLUMN_TYPES)
 
     return out
 
 
 
 if __name__ == '__main__':
+    # Download all-time NBA leaders data
     leaders = download_alltimeleaders()
-    CONNECTION = connect()
-    leaders.to_sql('ALLTIMELEADERS', CONNECTION, if_exists='replace')
+
+    # Save data to SQLite database
+    TABLE_NAME = 'ALLTIMELEADERS'
+    with connect() as conn:
+        leaders.to_sql(TABLE_NAME, conn, if_exists='replace')
