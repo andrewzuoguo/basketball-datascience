5f4d3ecbee9f2a32752b796264a1f8b76ed88b0f
andrewzuoguo <andrewzguo@gmail.com>
2023-04-15 17:15:45 -0400

    Add download_team_and_player_static_list.py file to download: - List of NBA Teams - List of Active NBA Players - List of Inactive NBA Players

---
+++ data/download_team_and_player_static_list.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from nba_api.stats.static import teams, players
+from connect_sqlite import connect
+
+def download_static(conn):
+    """
+    Downloads:
+    - List of NBA Teams
+    - List of Active Players
+    - List of Inactive Players
+    Saves to separate table in connected SQLite database
+
+    :param conn: Connection object representing the connection to the SQLite database
+    """
+    team_list = pd.DataFrame(teams.get_teams())
+    team_list = team_list.set_index('id')
+    team_list.to_sql('TEAM_LIST', conn, if_exists='replace')
+
+    active_player_list = pd.DataFrame(players.get_active_players())
+    active_player_list = active_player_list.set_index('id')
+    active_player_list.to_sql('PLAYER_LIST_ACTIVE', conn, if_exists='replace')
+
+    inactive_player_list = pd.DataFrame(players.get_inactive_players())
+    inactive_player_list = inactive_player_list.set_index('id')
+    inactive_player_list.to_sql('PLAYER_LIST_INACTIVE', conn, if_exists='replace')
+
+if __name__ == '__main__':
+    with connect() as conn:
+        download_static(conn)
+
+    print('Updated Team List')
+    print('Updated Active Player List')
+    print('Updated Inactive Player List')
